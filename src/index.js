d6dcb5ce08dbc188fb4cf1dd5ec4d6a9732e5cd9
Gregory Alpízar <gregory.ac@estudiantec.cr>
2021-11-06 19:16:58 -0600

    Working from Postman

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ const app = express(); // Inicia el servidor
 
 // config
 app.set('port', 3000);
+app.set('json spaces', 2);
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false})); // mensajes sencillos
+app.use(express.json());
 
 // API
 app.get('/getResponse', (req, res) => { // req = request, res = response. Esta es una forma de definir funciones es javascript
-    res.send('Hello from API');
+    //res.send('Hello from API');
+    res.json(
+        {
+            "name": "Gregory",
+            "age": 20
+        }
+    )
 });
 
 // Execute server
