7aea3337d6f09f51d3bf98003fac722d5ab6ce67
AlpizarGregory <gregory.ac@estudiantec.cr>
2021-11-08 18:10:44 -0600

    Delete function isn't working

--- src/routes/rt.js
+++ src/routes/rt.js
@@ -58,4 +58,64 @@ router.post('/addCourse', (req,res) => {
     }
 });
 
+// PUT - UPDATE
+router.put('/updateCourse/:key', (req, res) => {
+    const { course, code, grade } = req.body;
+    const { key } = req.params;
+
+    if(course && code && grade) {
+        try {
+            const jsonString = fs.readFileSync('src/db.json', 'utf-8');
+            const data = JSON.parse(jsonString);
+
+            data.forEach(element => {
+                if(element.code == key) {
+                    element.course = course;
+                    element.grade = grade;
+                }
+            });
+
+            const jsonData = JSON.stringify(data, null, 2);
+            fs.writeFile('src/db.json', jsonData, err => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log('Update Success');
+                    res.send('Update OK');
+                }
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    } else {
+        res.send('Attributes missing');
+    }
+});
+
+// DELETE
+router.delete('/deleteCourse/:key', (req,res) => {
+    const { key } = req.params;
+    try {
+        const jsonString = fs.readFileSync('src/db.json', 'utf-8');
+        const data = JSON.parse(jsonString);
+
+        for(var i = 0; i < data.lenght; i++) {
+            if(data[i].code == key) {
+                data.splice(i, 1);
+            }
+        }
+
+        const jsonData = JSON.stringify(data, null, 2);
+        fs.writeFile('src/db.json', jsonData, err => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send('Delete OK');
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
+
 module.exports = router;
